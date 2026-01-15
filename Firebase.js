815b89e0ba31bdc46c100e2790f3340a7440dfd5
Mangalam soni <mangalamsoni70@gmail.com>
2026-01-15 08:09:59 +0530

    Create Firebase.js

---
+++ Firebase.js
@@ -0,0 +1,19 @@
+<!-- Firebase SDK -->
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyAFlZeycy1fIMFB0qa9cMI8O83XhrPduSc",
+    authDomain: "bank-we.firebaseapp.com",
+    databaseURL: "https://bank-we-default-rtdb.firebaseio.com",
+    projectId: "bank-we",
+    storageBucket: "bank-we.firebasestorage.app",
+    messagingSenderId: "211837143984",
+    appId: "1:211837143984:web:d5615cf9c3a5c46a292475",
+    measurementId: "G-77DKPZB9K6"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+</script>
