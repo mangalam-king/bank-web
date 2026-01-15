f6e4fe02b1beccc825bd5bd61424d7bd696899d1
Mangalam soni <mangalamsoni70@gmail.com>
2026-01-15 08:18:41 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,4 +1,3 @@
-<!-- Firebase SDK -->
 <script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"></script>
 <script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"></script>
 <script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"></script>
@@ -11,8 +10,7 @@
     projectId: "bank-we",
     storageBucket: "bank-we.firebasestorage.app",
     messagingSenderId: "211837143984",
-    appId: "1:211837143984:web:d5615cf9c3a5c46a292475",
-    measurementId: "G-77DKPZB9K6"
+    appId: "1:211837143984:web:d5615cf9c3a5c46a292475"
   };
 
   firebase.initializeApp(firebaseConfig);
